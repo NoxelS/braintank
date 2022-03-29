524b51fb1ef1c575aeef7bf53c9c673bb59ec73b
Noel Schwabenland <noel@schwabenland.ch>
2022-03-29 22:34:22 +0200

    fixed fallback

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -6,40 +6,50 @@ import { getUserJsons, mainPool, storeUserJson } from '../lib/Store';
 const router = express.Router();
 
 router.get('/', async (req: Request, mainres: Response, next: NextFunction) => {
-    try {
-        getUserJsons((err, res) => {
-            if (err) {
-                next(err);
-            } else {
-                const users = res.map((r: any) => JSON.parse(r.json));
-                if (!users.find((user: any) => user.nickname == (req as any).oidc.user.nickname)) {
-                    storeUserJson(mainPool, JSON.stringify((req as any).oidc.user), (err, res) => {
-                        console.log(users);
-                        mainres.json((req as any).oidc.user);
-                    });
+    if ((req as any).oidc.user) {
+        try {
+            getUserJsons((err, res) => {
+                console.log((req as any).oidc.user);
+                if (err) {
+                    next(err);
                 } else {
-                    mainres.json((req as any).oidc.user);
+                    const users = res.map((r: any) => JSON.parse(r.json));
+                    console.log(users);
+                    if (users && !users.find((user: any) => user.picture == (req as any).oidc.user.picture)) {
+                        storeUserJson(mainPool, JSON.stringify((req as any).oidc.user), (err, res) => {
+                            console.log(users);
+                            mainres.json((req as any).oidc.user);
+                        });
+                    } else {
+                        mainres.json((req as any).oidc.user);
+                    }
                 }
-            }
-        });
-    } catch (err) {
-        next(err);
+            });
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        mainres.json([]);
     }
 });
 
 router.get('/all', async (req: Request, mainres: Response, next: NextFunction) => {
-    try {
-        getUserJsons((err, res) => {
-            if (res && !err) {
-                const users = res.map((r: any) => JSON.parse(r.json));
-                mainres.json(users);
-            } else {
-                console.log(err);
-                const users = [];
-            }
-        });
-    } catch (err) {
-        next(err);
+    if ((req as any).oidc.user) {
+        try {
+            getUserJsons((err, res) => {
+                if (res && !err) {
+                    const users = res.map((r: any) => JSON.parse(r.json));
+                    mainres.json(users);
+                } else {
+                    console.log(err);
+                    const users = [];
+                }
+            });
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        mainres.json([]);
     }
 });
 
